ea23686d3434ca04b75cb1af44c5b297f6f97319
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-01-27 19:09:14 +0100

    Refactor GPU ratio calculation in RunningModels component for improved readability and performance

--- components/RunningModels.jsx
+++ components/RunningModels.jsx
@@ -3,6 +3,7 @@
 import { useEffect, useState, useMemo } from "react";
 import { AnimatePresence, motion } from "motion/react";
 import { getOllamaClient } from "@/lib/ollamaClient";
+import { cn } from "@/lib/utils";
 
 const POLLING_INTERVAL = 5000;
 const ANIMATION_PROPS = {
@@ -38,15 +39,11 @@ export default function RunningModels() {
     };
   }, []);
 
-  const getGPURatio = useMemo(
-    () => (size, vram_size) => {
-      if (vram_size < size) {
-        return Math.floor((vram_size / size) * 100);
-      }
-      return 100;
-    },
-    [],
-  );
+  const getGPURatio = useMemo(() => {
+    if (!data.length) return null;
+    const { size, size_vram } = data[0];
+    return size_vram < size ? Math.floor((size_vram / size) * 100) : 100;
+  }, [data]);
 
   return (
     <AnimatePresence>
@@ -55,16 +52,33 @@ export default function RunningModels() {
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -20 }}
-          className="absolute top-5 border border-zinc-200 shadow px-3 py-1.5 rounded-full"
+          className="absolute top-5 border border-zinc-200 shadow px-3 py-1.5 rounded-full select-none"
         >
           <div className="flex items-center gap-2">
             <div className="relative flex size-2">
-              <span className="motion-safe:animate-ping absolute inline-flex h-full w-full rounded-full opacity-75 bg-green-500" />
-              <span className="relative inline-flex rounded-full size-2 opacity-90 bg-green-500" />
+              <span
+                className={cn(
+                  "motion-safe:animate-ping absolute inline-flex h-full w-full rounded-full opacity-75",
+                  getGPURatio > 90
+                    ? "bg-green-500"
+                    : getGPURatio > 50
+                      ? "bg-yellow-500"
+                      : "bg-red-500",
+                )}
+              />
+              <span
+                className={cn(
+                  "relative inline-flex rounded-full size-2 opacity-90 bg-green-500",
+                  getGPURatio > 90
+                    ? "bg-green-500"
+                    : getGPURatio > 50
+                      ? "bg-yellow-500"
+                      : "bg-red-500",
+                )}
+              />
             </div>
             <span className="text-xs text-zinc-900 font-semibold">
-              {data[0].name} - GPU{" "}
-              {getGPURatio(data[0].size, data[0].size_vram)}%
+              {data[0].name} - GPU {getGPURatio}%
             </span>
           </div>
         </motion.div>
